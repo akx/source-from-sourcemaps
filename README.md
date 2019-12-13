68e2601d4b013e383f05d0204249b224f7c85383
Aarni Koskela <akx@iki.fi>
2019-12-13 14:03:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+source-from-sourcemaps
+======================
+
+Extract original source from JavaScript sourcemaps.
+
+Usage
+-----
+
+```
+yarn
+node source-from-sourcemaps.js my.map
+```
+
+will write JS files under `sources-gen/`.
---
+++ source-from-sourcemaps.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const util = require("util");
+const writeFileP = util.promisify(fs.writeFile);
+const readFileP = util.promisify(fs.readFile);
+const mkdirpPromise = util.promisify(require("mkdirp"));
+const path = require("path");
+const { SourceMapConsumer } = require("source-map");
+const minimist = require("minimist");
+
+async function processSource(consumer, src) {
+  const fsSrc =
+    "sources-gen/" +
+    src
+      .replace(/webpack:\/\/\//, "")
+      .replace(/~\//, "node_modules/")
+      .replace(/\?.+$/, "");
+  const source = consumer.sourceContentFor(src, true);
+  if (!source) {
+    console.warn("Unable to source:", src);
+    return;
+  }
+  await mkdirpPromise(path.dirname(fsSrc));
+  await writeFileP(fsSrc, source, "UTF-8");
+  console.log(`Wrote ${fsSrc}`);
+}
+
+async function processFile(pth) {
+  const data = await readFileP(pth, "UTF-8");
+  return await SourceMapConsumer.with(data, null, async consumer => {
+    const processors = consumer.sources.map(src =>
+      processSource(consumer, src)
+    );
+    return await Promise.all(processors);
+  });
+}
+
+async function main() {
+  const args = minimist(process.argv.slice(2));
+  const processors = args._.map(pth => processFile(pth));
+  return await Promise.all(processors);
+}
+
+if (!module.parent) {
+  main().then(x => process.exit(0));
+}
